75fdbec51cfee2bb5104f0c250aebb07cb07d25a
Jonathan Bones <bonesyblue@gmail.com>
2020-08-16 18:41:55 +0200

    feat: Add scale lines to wheel

--- lib/wheel_painter.dart
+++ lib/wheel_painter.dart
@@ -20,6 +20,55 @@ class WheelPainter extends CustomPainter {
       radius,
       wheelBackgroundBrush,
     );
+
+    final coarseScaleCount = 24;
+    final coarseScaleInnerRadius = radius - 24;
+
+    drawScaleLines(
+      count: coarseScaleCount,
+      center: centerpoint,
+      outerRadius: radius,
+      innerRadius: coarseScaleInnerRadius,
+      canvas: canvas,
+    );
+
+    final fineScaleCount = coarseScaleCount * 5;
+    final fineScaleInnerRadius = radius - 12;
+
+    drawScaleLines(
+      count: fineScaleCount,
+      center: centerpoint,
+      outerRadius: radius,
+      innerRadius: fineScaleInnerRadius,
+      canvas: canvas,
+    );
+  }
+
+  void drawScaleLines({
+    @required int count,
+    @required Offset center,
+    @required double outerRadius,
+    @required double innerRadius,
+    @required Canvas canvas,
+  }) {
+    final double deltaAngle = (2 * pi) / count;
+    for (var i = 0; i <= count; i++) {
+      final alphaAngle = (i * deltaAngle);
+      final startOffsetY = center.dy + (outerRadius * sin(alphaAngle));
+      final startOffsetX = center.dx + (outerRadius * cos(alphaAngle));
+
+      final endOffsetY = center.dy + (innerRadius * sin(alphaAngle));
+      final endOffsetX = center.dx + (innerRadius * cos(alphaAngle));
+
+      final start = Offset(startOffsetX, startOffsetY);
+      final end = Offset(endOffsetX, endOffsetY);
+
+      final indicatorBrush = Paint()
+        ..color = Colors.white
+        ..strokeWidth = 1.0;
+
+      canvas.drawLine(start, end, indicatorBrush);
+    }
   }
 
   @override
