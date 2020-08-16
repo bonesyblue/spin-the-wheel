34953e316aa38e521f130bf6fe6ccf60e504fdd1
Jonathan Bones <bonesyblue@gmail.com>
2020-08-16 18:50:35 +0200

    feat: Wrap the wheel in a gesture detector widget

--- lib/wheel_spinner_page.dart
+++ lib/wheel_spinner_page.dart
@@ -1,20 +1,42 @@
 import 'package:SpinTheWheel/wheel_painter.dart';
 import 'package:flutter/material.dart';
 
+class WheelSpinnerPage extends StatefulWidget {
+  @override
+  State<WheelSpinnerPage> createState() => _WheelSpinnerPageState();
+}
+
 // Page to display the wheel painter
-class WheelSpinnerPage extends StatelessWidget {
+class _WheelSpinnerPageState extends State<WheelSpinnerPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: Container(
-          width: 250,
-          height: 250,
-          child: CustomPaint(
-            painter: WheelPainter(),
+      child: GestureDetector(
+        onPanStart: this.onPanStart,
+        onPanUpdate: this.onPanUpdate,
+        onPanEnd: this.onPanEnd,
+        child: Center(
+          child: Container(
+            width: 250,
+            height: 250,
+            child: CustomPaint(
+              painter: WheelPainter(),
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void onPanStart(DragStartDetails details) {
+    debugPrint("Pan start: $details");
+  }
+
+  void onPanUpdate(DragUpdateDetails details) {
+    debugPrint("Pan update: $details");
+  }
+
+  void onPanEnd(DragEndDetails details) {
+    debugPrint("Pan end: $details");
   }
 }
